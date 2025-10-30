fe8cda2d4ea774a06ad4fe8e272e4c74a7375203
Armaan5628 <armaansandher22@gmail.com>
2025-10-29 23:22:52 -0400

    Publish web with correct base href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4216847943"
+    serviceWorkerVersion: "2036305233"
   }
 });
